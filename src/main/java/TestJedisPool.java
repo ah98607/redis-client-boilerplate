7f5f31be1c7667676bd67bd07530b7e849b747eb
ah98607 <adh6107@gmail.com>
2020-06-28 08:23:53 -0700

    more data types

--- src/main/java/TestJedisPool.java
+++ src/main/java/TestJedisPool.java
@@ -1,5 +1,8 @@
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import java.util.*;
+
+import java.util.HashMap;
 
 public class TestJedisPool {
 
@@ -13,8 +16,59 @@ public class TestJedisPool {
         Jedis jedis = null;
         try {
             jedis = pool.getResource();
-            jedis.set("name", "Andy");
-            System.out.println("Value received from Redis: " + jedis.get(" name"));
+
+            // string type
+            jedis.set("stringData", "hello");
+            System.out.println("Value received from Redis: " + jedis.get(" stringData"));
+            if (!jedis.exists("unspecifiedKey")) {
+                System.out.println("Key not found");
+            }
+
+            // hash type
+            Map<String, String> userData = new HashMap<String, String>();
+            userData.put("country", "USA");
+            userData.put("state", "Washington");
+            userData.put("city", "Seattle");
+            jedis.hset("hashData", userData);
+            System.out.print("Keys in this hash ");
+            for (String key : jedis.hkeys("hashData")) {
+                System.out.print(key + " ");
+            }
+            System.out.print("Values in this hash ");
+            for (String key : jedis.hvals("hashData")) {
+                System.out.print(key + " ");
+            }
+            Map<String, String> outputHashData = jedis.hgetAll("hashData");
+            for (Map.Entry<String, String> entry : outputHashData.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+            jedis.hset("hashData", "city", "Camas");
+            System.out.println("Changed city: " + jedis.hget("hashData", "city"));
+
+            // list
+            for (int i = 0; i < 5; i++) {
+                jedis.lpush("listData", "listItem" + i);
+            }
+            for (int i = 0; i < 5; i++) {
+                jedis.rpush("listData", "listItem" + String.valueOf(i + 5));
+            }
+            System.out.print("left pop: ");
+            for (int i = 0; i < 5; i++) {
+                System.out.print(jedis.lpop("listData") + " ");
+            }
+            System.out.print("\nright pop");
+            for (int i = 0; i < 5; i++) {
+                System.out.print(jedis.rpop("listData") + " ");
+            }
+
+            // set
+            for (int i = 0; i < 5; i++) {
+                jedis.sadd("setData", "setItem" + i);
+            }
+            System.out.print("\nset members: ");
+            for (String setItem : jedis.smembers("setData")) {
+                System.out.print(setItem + " ");
+            }
         }
         catch (Exception e) {
             System.out.println(e);
